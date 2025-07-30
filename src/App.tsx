5e7891a13c51406ddee74d00935a7d0409b96904
Disha Chakraborty <chakrabortydisha809@gmail.com>
2025-07-30 19:18:38 +0530

    Delete src directory

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-<img
-  src="/healthcare.jpg"
-  alt="Pharma Chat Logo"
-  className="h-12 w-auto mx-auto mb-4"
-/>
-
-<h1 className="text-xl font-bold text-center">Pharma Chat</h1>
-<p className="text-sm text-gray-500 text-center">Consultation Assistant</p>
